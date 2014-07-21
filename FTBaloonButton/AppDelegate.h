a2a64d49deb1de0145b2870ff951c9c0748177b2
Andre Hoffmann <contact@effzehn.de>
2014-07-21 12:39:00 +0200

    Initial Commit

---
+++ FTBaloonButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FTBaloonButton
+//
+//  Created by Andre Hoffmann on 21.07.14.
+//  Copyright (c) 2014 Andre Hoffmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FTBaloonButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FTBaloonButton
+//
+//  Created by Andre Hoffmann on 21.07.14.
+//  Copyright (c) 2014 Andre Hoffmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
